020e8f746bdd74267cb548cfc169282a1733eed2
saurav11sarkar <sarkarsaurav11@outlook.com>
2025-10-21 15:47:36 +0600

    active user add

--- src/entities/user/user.service.js
+++ src/entities/user/user.service.js
@@ -373,3 +373,34 @@ export const deleteUserPDF = async (id) => {
   return updatedUser;
 };
 
+
+
+export const getMonthlyActiveUsers = async (year) => {
+  const users = await User.aggregate([
+    {
+      $match: {
+        createdAt: {
+          $gte: new Date(`${year}-01-01`),
+          $lte: new Date(`${year}-12-31`),
+        },
+      },
+    },
+    {
+      $group: {
+        _id: { $month: '$createdAt' },
+        totalUsers: { $sum: 1 },
+      },
+    },
+    { $sort: { _id: 1 } },
+  ]);
+
+  const months = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December',
+  ];
+
+  return months.map((month, i) => {
+    const found = users.find((u) => u._id === i + 1);
+    return { month, totalUsers: found ? found.totalUsers : 0 };
+  });
+};
