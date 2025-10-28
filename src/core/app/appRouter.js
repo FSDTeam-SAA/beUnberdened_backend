a1b9b9b73d1ec5632f369aa7575b188a7947f803
Wrong1234 <mahabur24cse@gmail.com>
2025-10-28 14:21:35 +0600

    fix everything

--- src/core/app/appRouter.js
+++ src/core/app/appRouter.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import authRoutes from '../../entities/auth/auth.routes.js';
-import userRoutes from '../../entities/user/user.routes.js';
 import profileRoutes from '../../entities/profile/profileRoutes.js';
 import blogRoutes from '../../entities/blog/blogRoutes.js';
 import serviceRoutes from '../../entities/service/serviceRoutes.js';
@@ -14,7 +13,6 @@ const router = express.Router();
 
 
 router.use('/v1/auth', authRoutes);
-router.use('/v1/user', userRoutes);
 router.use('/v1/profile', profileRoutes);
 router.use('/v1/blogs', blogRoutes);
 router.use('/v1/services', serviceRoutes);
